c9c51a900b6aac5dead3db19d92c63450801c54d
skorohod <132@mail.ru>
2023-08-18 16:34:19 +0300

    homework

--- app_advertisiments/models.py
+++ app_advertisiments/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
 
+
+
+
+
+
 class Advertisements(models.Model):
     title = models.CharField('Заголовок', max_length=128)
     description = models.TextField('Описание')
@@ -9,3 +14,9 @@ class Advertisements(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return f"Advertisements(id={self.id}, title={self.title}, price={self.price})"
+
+
+    class Meta:
+        db_table = 'advertisements'
