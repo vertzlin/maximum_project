bfd1a201666397d888fe6ee5114af71c19809ab1
skorohod <132@mail.ru>
2023-08-25 13:55:30 +0300

    название

--- app_advertisiments/urls.py
+++ app_advertisiments/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import index, top_sellers
+from .views import index, top_sellers, advertisement_post
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('', index, name='main-page'),
-    path('top-sellers', top_sellers, name='top-sellers')
+    path('top-sellers', top_sellers, name='top-sellers'),
+    path('advertisement-post', advertisement_post, name='adv-post')
 ]
 
 
--- app_advertisiments/views.py
+++ app_advertisiments/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Advertisements
+from .forms import AdvertisementForm
+from django.urls import reverse
+
 
 def index(request):
     advertisiments = Advertisements.objects.all()
@@ -8,3 +11,18 @@ def index(request):
 
 def top_sellers(request):
     return render(request, 'top-sellers.html')
+
+
+def advertisement_post(request):
+    if request.method == "POST":
+        form = AdvertisementForm(request.POST, request.FILES)
+        if form.is_valid():
+            advertisement = Advertisements(**form.cleaned_data)
+            advertisement.user = request.user
+            advertisement.save()
+            url = reverse('main-page')
+            return redirect(url)
+    else:
+        form = AdvertisementForm
+    context = {'form': form}
+    return render(request, 'advertisement-post.html', context)
